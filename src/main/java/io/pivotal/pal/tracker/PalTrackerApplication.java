cdfae023e4b3d6d993a370a410918a11f4614715
sashinpivotal <sashin@pivotal.io>
2018-11-20 16:32:59 -0500

    Lab set to solution

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -10,26 +10,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import javax.sql.DataSource;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class PalTrackerApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(PalTrackerApplication.class, args );
+        // Make sure the application runs as UTC
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(PalTrackerApplication.class, args);
     }
 
     @Bean
-    JdbcTimeEntryRepository timeEntryRepository(DataSource dataSource){
+    TimeEntryRepository timeEntryRepository(DataSource dataSource) {
         return new JdbcTimeEntryRepository(dataSource);
     }
 
     @Bean
-    public ObjectMapper jsonObjectMapper(){
+    public ObjectMapper jsonObjectMapper() {
         return Jackson2ObjectMapperBuilder.json()
                 .serializationInclusion(JsonInclude.Include.NON_NULL) // Don’t include null values
                 .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS) //ISODate
                 .modules(new JavaTimeModule())
                 .build();
     }
-
-
 }
